5b58a1199fc78547e7566ff68288e9e6227c2e98
Heinz <hwbusch@uc.cl>
2023-08-08 03:14:10 -0400

    test change

--- src/weather.js
+++ src/weather.js
@@ -10,7 +10,7 @@ async function getWeather(city) {
     );
     const data = response.data;
     const cityResponse = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
-    return `${cityResponse}\n${data.current.condition.text}\nAhora: ${data.current.temp_c}°C\nMax: ${data.forecast.forecastday[0].day.maxtemp_c}°C\nMin: ${data.forecast.forecastday[0].day.mintemp_c}°C\nProb de lluvia: ${data.forecast.forecastday[0].day.daily_chance_of_rain}%`;
+    return `${cityResponse}\n${data.current.condition.text}\nAhora: ${data.current.temp_c}°C\nMax: ${data.forecast.forecastday[0].day.maxtemp_c}°C\nMin: ${data.forecast.forecastday[0].day.mintemp_c}°C\nProb de lluvia: ${data.forecast.forecastday[0].day.daily_chance_of_rain}% xd`;
   } catch (error) {
     return `No se pudo obtener el clima de ${city}`;
   }
