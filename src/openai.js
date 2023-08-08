cff0b05bee2dad502876ea67f284c00c4fa44691
Heinz <hwbusch@uc.cl>
2023-08-08 11:35:44 -0400

    deploy to raspi

--- src/openai.js
+++ src/openai.js
@@ -7,3 +7,39 @@ const configuration = new Configuration({
 
 const openai = new OpenAI(configuration);
 
+async function askGPT(prompt) {
+  try {
+    const chatCompletion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+  
+    return chatCompletion;  
+  } catch (error) {
+    return "Hubo un error.";
+  }
+}
+
+async function gptResponse(message) {
+  try {
+    const parts = message.body.split(' ');
+    if (parts.length < 2) {
+      return;
+    }
+    const prompt = parts.slice(1).join(' ');
+    const response = await askGPT(prompt);
+    if (message.fromMe) {
+      client.sendMessage(message.to, response);    
+    } else {
+      const chat = await message.getChat();
+      const contact = await message.getContact();
+      await chat.sendMessage(`@${contact.id.user}, ${response}`, {
+          mentions: [contact]
+      });    
+    }
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+module.exports = gptResponse; 
--- src/weather.js
+++ src/weather.js
@@ -16,4 +16,22 @@ async function getWeather(city) {
   }
 }
 
-module.exports = getWeather;
+async function weatherResponse(message){
+  const parts = message.body.split(' ');
+  if (parts.length < 2) {
+    return;
+  }
+  const cityName = parts.slice(1).join(' ');
+  const weatherInfo = await getWeather(cityName);
+  if (message.fromMe) {
+    client.sendMessage(message.to, weatherInfo);    
+  } else {
+    const chat = await message.getChat();
+    const contact = await message.getContact();
+    await chat.sendMessage(`@${contact.id.user}, ${weatherInfo}`, {
+        mentions: [contact]
+    });    
+  }
+}
+
+module.exports = weatherResponse;
