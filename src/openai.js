a308585e4d93e993d689393cea9ecee10259c643
Heinz <hwbusch@uc.cl>
2023-08-08 01:25:42 -0400

    feat: added docker

---
+++ src/openai.js
@@ -0,0 +1,9 @@
+const { Configuration, OpenAI } = require('openai');
+require('dotenv').config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAI(configuration);
+
