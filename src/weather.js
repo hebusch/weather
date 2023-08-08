7f3b45e88099daaa80576a431d67f6147aabb4a5
Heinz <hwbusch@uc.cl>
2023-08-08 00:55:47 -0400

    fix: fixed error when city not found

--- src/weather.js
+++ src/weather.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 async function getWeather(city) {
+  try {
     const cityFormatted = city.split(' ').join('%20');
     const response = await axios.get(
       `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${cityFormatted}&lang=es`
@@ -10,6 +11,9 @@ async function getWeather(city) {
     const data = response.data;
     const cityResponse = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
     return `${cityResponse}\nCondición: ${data.current.condition.text}\nTemperatura actual: ${data.current.temp_c}°C\nTemperatura máxima: ${data.forecast.forecastday[0].day.maxtemp_c}°C\nTemperatura mínima: ${data.forecast.forecastday[0].day.mintemp_c}°C`;
+  } catch (error) {
+    return `No se pudo obtener el clima de ${city}`;
+  }
 }
 
 module.exports = getWeather;
