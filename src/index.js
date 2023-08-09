ea49affd869825b5b35714caee8a027a18d208dd
Heinz <hwbusch@uc.cl>
2023-08-09 02:35:41 -0400

    deploy to raspi

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 const client = require('./client');
 const weatherResponse = require('./cmds/weather');
 const { gptResponse, gptImage } = require('./cmds/openai');
-// const horoscopeResponse = require('./cmds/horoscopo');
+const fastq = require('fastq');
+
+// Función de trabajo para la cola
+function taskHandler(message, cb) {
+  if (message.body.toLowerCase().startsWith('/ask')) {
+    gptResponse(message, client).then(() => cb()).catch(error => {
+      console.error('Error in gptResponse:', error);
+      cb(error);
+    });
+  } else if (message.body.toLocaleLowerCase().startsWith('/img')) {
+    gptImage(message, client).then(() => cb()).catch(error => {
+      console.error('Error in gptImage:', error);
+      cb(error);
+    });
+  } else if (message.body.toLowerCase().startsWith('weather')) {
+    weatherResponse(message, client).then(() => cb()).catch(error => {
+      console.error('Error in weatherResponse:', error);
+      cb(error);
+    });
+  } else {
+    cb();
+  }
+}
+
+// Crea la cola con un worker y concurrencia 1
+const queue = fastq.promise(taskHandler, 1);
 
 client.on('message_create', async (message) => {
-  if (message.body.toLowerCase().startsWith('/ask')) {
-    gptResponse(message, client).catch(error => console.error('Error in gptResponse:', error));
-  }
-
-  if (message.body.toLocaleLowerCase().startsWith('/img')) {
-    gptImage(message, client).catch(error => console.error('Error in gptImage:', error));
-  }
-
-  if (message.body.toLowerCase().startsWith('weather')) {
-    weatherResponse(message, client).catch(error => console.error('Error in weatherResponse:', error));
-  }
-
-  // if (message.body.toLowerCase().startsWith('/horoscopo')) {
-  //   await horoscopeResponse(message, client);
-  // }
+  queue.push(message);
 });
