8cbbfd379fcbe953b0c631d340ea35d5353fc396
Heinz <hwbusch@uc.cl>
2023-08-09 03:04:15 -0400

    deploy to raspi

--- src/index.js
+++ src/index.js
@@ -4,22 +4,43 @@ const { gptResponse, gptImage } = require('./cmds/openai');
 const fastq = require('fastq');
 
 function taskHandler(message, cb) {
+  console.log('Procesando mensaje:', message.body);
+
   if (message.body.toLowerCase().startsWith('/ask')) {
-    gptResponse(message, client).then(() => cb()).catch(error => {
-      console.error('Error in gptResponse:', error);
-      cb(error);
-    });
-  } else if (message.body.toLocaleLowerCase().startsWith('/img')) {
-    gptImage(message, client).then(() => cb()).catch(error => {
-      console.error('Error in gptImage:', error);
-      cb(error);
-    });
-  } else if (message.body.toLowerCase().startsWith('weather')) {
-    weatherResponse(message, client).then(() => cb()).catch(error => {
-      console.error('Error in weatherResponse:', error);
-      cb(error);
-    });
-  } else {
+    gptResponse(message, client)
+      .then(() => {
+        console.log('gptResponse procesado con éxito.');
+        cb();
+      })
+      .catch(error => {
+        console.error('Error en gptResponse:', error);
+        cb(error);
+      });
+  } 
+  else if (message.body.toLowerCase().startsWith('/img')) {
+    gptImage(message, client)
+      .then(() => {
+        console.log('gptImage procesado con éxito.');
+        cb();
+      })
+      .catch(error => {
+        console.error('Error en gptImage:', error);
+        cb(error);
+      });
+  } 
+  else if (message.body.toLowerCase().startsWith('weather')) {
+    weatherResponse(message, client)
+      .then(() => {
+        console.log('weatherResponse procesado con éxito.');
+        cb();
+      })
+      .catch(error => {
+        console.error('Error en weatherResponse:', error);
+        cb(error);
+      });
+  } 
+  else {
+    console.log('Mensaje no reconocido:', message.body);
     cb();
   }
 }
@@ -27,5 +48,6 @@ function taskHandler(message, cb) {
 const queue = fastq.promise(taskHandler, 1);
 
 client.on('message_create', async (message) => {
+  console.log('Mensaje recibido:', message.body);
   queue.push(message);
 });
