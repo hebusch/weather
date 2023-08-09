2c75db3b6d3aeebd3b91c454c3550bd4dca31b73
Heinz <hwbusch@uc.cl>
2023-08-09 02:42:43 -0400

    deploy to raspi

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const weatherResponse = require('./cmds/weather');
 const { gptResponse, gptImage } = require('./cmds/openai');
 const fastq = require('fastq');
 
-// Función de trabajo para la cola
 function taskHandler(message, cb) {
   if (message.body.toLowerCase().startsWith('/ask')) {
     gptResponse(message, client).then(() => cb()).catch(error => {
@@ -25,7 +24,6 @@ function taskHandler(message, cb) {
   }
 }
 
-// Crea la cola con un worker y concurrencia 1
 const queue = fastq.promise(taskHandler, 1);
 
 client.on('message_create', async (message) => {
