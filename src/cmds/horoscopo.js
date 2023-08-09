456fc8c1b3c285ec6ac3fdd7e89d0ce1d8034b65
Heinz <hwbusch@uc.cl>
2023-08-08 21:00:58 -0400

    deploy to raspi

---
+++ src/cmds/horoscopo.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+
+function todayDate() {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${month}-${day}-${year}`;
+}
+
+async function getHoroscope(sign) {
+  try {
+    const date = todayDate();
+    const url = `https://us-central1-tf-natal.cloudfunctions.net/horoscopeapi_test?sign=${sign}&date=${date}`;
+    const response = await axios.get(url);
+    console.log(response.data);
+  } catch (error) {
+    return "No se ha podido procesar la solicitud.";
+  }
+}
+
+async function horoscopeResponse(message, client) {
+  try {
+    const parts = message.body.split(' ');
+    if (parts.length < 2) {
+      return;
+    }
+    const sign = parts[1];
+    const response = await getHoroscope(sign);
+    if (message.fromMe) {
+      client.sendMessage(message.to, response);    
+    } else {
+      const chat = await message.getChat();
+      const contact = await message.getContact();
+      await chat.sendMessage(`@${contact.id.user}, ${response}`, {
+          mentions: [contact]
+      });    
+    }
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+module.exports = horoscopeResponse;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const client = require('./client');
 const weatherResponse = require('./cmds/weather');
 const gptResponse = require('./cmds/openai');
+const horoscopeResponse = require('./cmds/horoscopo');
 
 client.on('message_create', async (message) => {
   if (message.body.startsWith('/ask')) {
@@ -9,5 +10,9 @@ client.on('message_create', async (message) => {
 
   if (message.body.toLowerCase().startsWith('weather')) {
     await weatherResponse(message, client);
+  }
+
+  if (message.body.toLowerCase().startsWith('/horoscopo')) {
+    await horoscopeResponse(message, client);
   }
 });
